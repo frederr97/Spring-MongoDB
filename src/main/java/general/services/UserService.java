2b407578be9d0384ebcab7f6b92e2e266edf7249
fredribeiro97@gmail.com <fredribeiro97@gmail.com>
2019-01-16 12:22:14 -0200

    User insertion

--- src/main/java/general/services/UserService.java
+++ src/main/java/general/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import general.domain.User;
+import general.dto.UserDTO;
 import general.repository.UserRepository;
 import general.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
