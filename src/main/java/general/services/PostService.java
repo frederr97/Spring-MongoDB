b0d8d8d15b6249bc01db1f6495e312442aa629eb
fredribeiro97@gmail.com <fredribeiro97@gmail.com>
2019-01-16 16:10:16 -0200

    Query methods to post

---
+++ src/main/java/general/services/PostService.java
@@ -0,0 +1,27 @@
+package general.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import general.domain.Post;
+import general.repository.PostRepository;
+import general.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
+}
